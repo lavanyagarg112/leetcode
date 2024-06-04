47cb3fcc51f2d96561f309fbbc23f9528adb60ce
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-06-04 17:15:32 +0800

    Contains Duplicate

---
+++ easy/containsDuplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        d = {}
+
+        for i in nums:
+            if i in d:
+                return True
+            d[i] = 1
+        return False
