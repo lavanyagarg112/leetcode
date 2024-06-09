bc7aa1171747a92acfec00503b8759e8dd9ccd59
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-06-09 20:43:23 +0800

    leetcode soln added

--- medium/LongestSubstringWithoutRepeatingCharacters.py
+++ medium/LongestSubstringWithoutRepeatingCharacters.py
@@ -26,3 +26,19 @@ def lengthOfLongestSubstring(s):
 print(lengthOfLongestSubstring("dvdf"))
 
 # need to figure out how to do better
+
+# 2 pointer solution from leetcode:
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left = max_length = 0
+        char_set = set()
+        
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+
+            char_set.add(s[right])
+            max_length = max(max_length, right - left + 1)
+        
+        return max_length
